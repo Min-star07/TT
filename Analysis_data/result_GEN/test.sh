543eacc5076dbce829b794964c00b05fc1dbe7d0
Min-star07 <min.li@iphc.cnrs.fr>
2024-03-22 18:27:27 +0100

    new

--- Analysis_data/result_GEN/test.sh
+++ Analysis_data/result_GEN/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Simple for loop example
-numbers=(15)
+numbers=(5)
 for i in "${numbers[@]}"
 do
     echo "Iteration: $i"
       # ./RunFitter -mode 2 -cb 22 -ROB $i -time 6 -led ./Data/20240116/led_cCB-22_2024-01-16_20_37_hist.root -ped ./Data/pedestal/ped_cCB-22_2024-01-09_17_40_hist.root -channel_start 0 -channel_end 64
       # ./RunFitter -mode 1 -cb 22 -ROB $i -led ../../Data/20240305/Wilki_led_22C4_cCB-22_2024-03-06_10_48_hist.root -ped ../../Data/20240305/Wilki_ped_cCB-22_2024-03-05_16_35_hist.root -channel_start 0 -channel_end 64
-      ./RunFitter -mode 2 -cb 22 -ROB $i -time 6 -led ./Data/20240116/led_cCB-22_2024-01-16_20_37_hist.root -ped ./Data/pedestal/ped_cCB-22_2024-01-09_17_40_hist.root -channel_start 0 -channel_end 64
-      # ./RunFitter -mode 1 -cb 22 -ROB $i -led ../../Data/20240305/Wilki_led_22C4_cCB-22_2024-03-06_10_48_hist.root -ped ../../Data/20240305/Wilki_ped_cCB-22_2024-03-05_16_35_hist.root -channel_start 0 -channel_end 64
+      # ./RunFitter -mode 2 -cb 22 -ROB $i -time 6 -led ./Data/20240116/led_cCB-22_2024-01-16_20_37_hist.root -ped ./Data/pedestal/ped_cCB-22_2024-01-09_17_40_hist.root -channel_start 0 -channel_end 64
+      ./RunFitter -mode 2 -cb 22 -ROB $i -led ./Data/20240322/norm_cCB-22_2024-03-22_14_50_hist.root -ped ./Data/20240322/FADC_ped_cCB-22_2024-03-05_16_32_hist.root -channel_start 0 -channel_end 64
 done
